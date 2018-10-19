e2e775bd4a4ec4ffe2dea40f96d860d6e3ecbb69
Jared Reando <jaredreando@gmail.com>
2018-10-19 16:43:57 -0700

    Add additional question/response gates. Edit text for clarity.

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     var name = $("#name").val();
     var qMobile =  $("input:radio[name=qMobile]:checked").val();
     var qInstituion =  $("input:radio[name=qInstituion]:checked").val();
-    var questionThree =  $("input:radio[name=questionThree]:checked").val();
-    var questionFour =  $("input:radio[name=questionFour]:checked").val();
+    var qStyle =  $("input:radio[name=qStyle]:checked").val();
+    var qApplication =  $("input:radio[name=qApplication]:checked").val();
     var questionFive =  $("input:radio[name=questionFive]:checked").val();
 
   // User feedback. Takes variables set above, which pull values from form elements on index.html after 'submit' is activated.
@@ -15,12 +15,21 @@ $(document).ready(function() {
   // Example: something like '<span class="ansMobile"></span>" must exist on index.html in order for the jQuery fucntion
   // "$(.mobile).text(mobile);" to sucessfully return a text result of the user's input on the index.html page.
 console.log(qMobile);
+console.log(qApplication);
+
+    $(".name").text(name);
 
     if (name === "") {
       alert("Whoa there, stranger! Let's have a name, eh?");
+    } else if (qMobile || qInstituion === "undefined") {
+      alert("Looks like you missed a question!");
+    } else if (qMobile === "1" && qApplication === "2") {
+      $("#android").show();
+    } else if (qInstituion === "3" && qMobile != "1") {
+      $("#dotNet").show();
+    } else {
+      $("#copOut").show();
     }
 
-    $(".name").text(name);
-    $("p#hide").toggleClass();
   });
 });
