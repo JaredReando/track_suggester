bdd02427e7c50d46b2f963fd94596756137d4c7f
Jared Reando <jaredreando@gmail.com>
2018-10-19 12:21:34 -0700

    Change form responses to all radio buttons, remove grid layout.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
   $("#survey").submit(function(event) {
-  event.preventDefault();
-
-  var ansMobile =  $("input:radio[name=mobile]:checked").val();
-  var companySize = $("#companySize").val();
-  var learningStyle = $("#learningStyle").val();
-  var whichEnd = $("#whichEnd").val();
+    event.preventDefault();
 
 
-// User feedback. Takes variables set above, which pull values from form elements on index.html after 'submit' is activated.
-// Passes these values into the print fucntions below.
-// Matching classes must be set on index.html in order to return variable value into an awaiting response area.
-// Example: something like '<span class="ansMobile"></span>" must exist on index.html in order for the jQuery fucntion
-// "$(.mobile).text(mobile);" to sucessfully return a text result of the user's input on the index.html page.
+    var ansMobile =  $("input:radio[name=mobile]:checked").val();
+    var questionOne = $("#questionOne").val();
+    var questionTwo = $("#questionTwo").val();
+    var questionThree = $("#questionThree").val();
+    var questionFour = $("#questionFour").val();
+    var questionFive = $("#questionFive").val();
 
+  // User feedback. Takes variables set above, which pull values from form elements on index.html after 'submit' is activated.
+  // Passes these values into the print fucntions below.
+  // Matching classes must be set on index.html in order to return variable value into an awaiting response area.
+  // Example: something like '<span class="ansMobile"></span>" must exist on index.html in order for the jQuery fucntion
+  // "$(.mobile).text(mobile);" to sucessfully return a text result of the user's input on the index.html page.
 
+    $(".questionFive").html("<h3>questionFive</h3>");
+    $("p#hide").toggleClass();
+  });
 });
